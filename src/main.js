610652ba485f60d890e9b7c02c6f7b315d0a97f5
netocalderon <netocalderon.be@gmail.com>
2023-11-18 19:17:31 +0100

    Upload excersies

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 // Add your JavaScript here
+
+const display = document.getElementById("inputBox");
+const buttons = document.querySelectorAll(".number, .operator, #clear, #equal, #backspace");
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                if (button.id === "clear") {
+                    display.value = "";
+                }
+                else if (button.id === "equal") {
+                    try {
+                        display.value = eval(display.value);
+                    }
+                    catch (error) {
+                        display.value = "Error!!!";
+                    }
+                } else if (button.id === "backspace") {
+                    display.value = display.value.slice(0, -1);
+
+                }
+                else {
+                    display.value += button.textContent;
+                }
+            });
+        });
